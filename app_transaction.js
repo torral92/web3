42334a8c96ad44e73afacc235cf0077beab38638
torral92 <78560824+torral92@users.noreply.github.com>
2023-04-25 12:36:57 +0200

    Update app_transaction.js

--- app_transaction.js
+++ app_transaction.js
@@ -1,13 +1,13 @@
 const Web3 = require('web3')
 const EthereumTx = require('ethereumjs-tx') // Permite el empaquetado y firma.
 const ethTx = require('ethereumjs-tx').Transaction
-const web3 = new Web3('https://sepolia.infura.io/v3/de67cbd819264018b84ecc260989a01f')
+const web3 = new Web3('https://sepolia.infura.io/v3/xxxxxxxxx')
 
 const address1 = '0x6D3b0C9E6235B287187A0C5086BDF094AB880357'
 const address2 = '0x178b7935cDc070970b507c96C9D683b0E72Adcf9'
 
-const address1Key = Buffer.from('c826357b3eb227fbae4318e9ec56c5cf00d9b82a3944532605ccd286fd334296','hex')
-const address2Key = Buffer.from('e3ab031d9bb644763e2ceaacdce2c2d065d35103570e152029c7148f5306df8d','hex')
+const address1Key = Buffer.from('xxxxxxxx','hex')
+const address2Key = Buffer.from('xxxxxxxx','hex')
 
 //Numero de transaccion para una address determinada, enviamos de la address uno a la 2.
 
